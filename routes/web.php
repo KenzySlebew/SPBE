f339dab928c487d5bb1bfe10398471fb54ffe57d
M. Rif'at Arrahman <113071777+KenzySlebew@users.noreply.github.com>
2025-01-08 09:27:50 +0800

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\NewsController;
 use App\Http\Controllers\AnnouncementController;
 use App\Http\Controllers\InfographicController;
+use App\Http\Controllers\ProfilSPBEController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,6 +23,7 @@ Route::get('/', function () {
 });
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::resource('news', NewsController::class);
-Route::resource('announcements', AnnouncementController::class);
-Route::resource('infographics', InfographicController::class);
+// Route::resource('news', NewsController::class);
+// Route::resource('announcements', AnnouncementController::class);
+// Route::resource('infographics', InfographicController::class);
+Route::get('/profil-spbe', [ProfilSPBEController::class, 'profile'])->name('profil.spbe');
